c511bad94c7f1bd4f9f23ffe9f3b84878ae03ed4
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-30 23:25:24 +0200

    Adding data information from an array

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { FontAwesomeIcon, faLanguage, faCartShopping, faCloudArrowDown, faPlane, faChartPie, faWordPress } from '@fortawesome/react-fontawesome'
 
-const dataInfo = [{image: faWordPress}, {image: faCartShopping},{image: faCloudArrowDown}, {image: faLanguage}, {image: faPlane}, {image: faChartPie}]
+
+
+const dataInfo = [
+  {title: "Lorem ipsum dolor", icon: faWordPress}, 
+  {title: "Consectetur adipiscing", icon: faCartShopping},
+  {title: "Lorem ipsum dolor", icon: faCloudArrowDown}, 
+  {title: "Consectetur adipiscing", icon: faLanguage}, 
+  {title: "Lorem ipsum dolor", icon: faPlane}, 
+  {title: "Consectetur adipiscing", icon: faChartPie}
+]
+
+console.log(dataInfo)
 
 
 const Services = () => {
@@ -12,8 +23,8 @@ const Services = () => {
         {dataInfo.map((item, index) => (
           <div key={index}>
               <div>
-                < FontAwesomeIcon icon={item}/>
-                <h1></h1>
+                < FontAwesomeIcon icon={item.icon}/>
+                <h1>{item.title}</h1>
               </div>
           </div>
         ))}
